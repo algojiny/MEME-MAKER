375f17f0a87fdc03920fadb4c7bc1f963a9b5ebb
Jiny <jiny8510@gmail.com>
2022-10-28 16:43:51 +0900

    #2.2 Line Width (07:10)

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const canvas = document.querySelector("canvas");
+const lineWidth = document.getElementById("line-width");
+const showLineWidth = document.getElementById("show-line-width");
 const ctx = canvas.getContext("2d"); //그림을 그릴때 붓처럼 사용하는것 context
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
-
+ctx.lineWidth = lineWidth.value;
+showLineWidth.innerText = lineWidth.value + "px";
 let isPainting = false;
+
 // const colors = ["#ff3838", "#ffb8b8", "#c56cf8", "#ff9f1a", "#fff200", "#32ff7e", "#7efff5", "#18dcff", "#7d5fff"];
 // const color = colors[Math.floor(Math.random() * colors.length)];
 // ctx.strokeStyle = color;
@@ -17,16 +20,24 @@ function onMove(event) {
     ctx.stroke();
     return;
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 }
-function startPainting(event) {
+function startPainting() {
   isPainting = true;
 }
-function cancelPainting(event) {
+function cancelPainting() {
   isPainting = false;
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+  showLineWidth.innerText = event.target.value + "px";
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); //마우스가 캔버스를 벗어나면 그리기모드 취소
+
+lineWidth.addEventListener("change", onLineWidthChange);
