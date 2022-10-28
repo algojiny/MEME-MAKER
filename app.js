b0761aaf13ccd22ee5ad5e7fda9e41ba1e59b852
Jiny <jiny8510@gmail.com>
2022-10-28 14:38:43 +0900

    #2.1 Mouse Painting (09:08)

--- app.js
+++ app.js
@@ -4,15 +4,29 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
 
-const colors = ["#ff3838", "#ffb8b8", "#c56cf8", "#ff9f1a", "#fff200", "#32ff7e", "#7efff5", "#18dcff", "#7d5fff"];
+let isPainting = false;
+// const colors = ["#ff3838", "#ffb8b8", "#c56cf8", "#ff9f1a", "#fff200", "#32ff7e", "#7efff5", "#18dcff", "#7d5fff"];
+// const color = colors[Math.floor(Math.random() * colors.length)];
+// ctx.strokeStyle = color;
+// ctx.lineTo(event.offsetX, event.offsetY);
+// ctx.stroke();
 
-function onClick(event) {
-  ctx.beginPath();
-  ctx.moveTo(400, 400);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting(event) {
+  isPainting = true;
+}
+function cancelPainting(event) {
+  isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting); //마우스가 캔버스를 벗어나면 그리기모드 취소
