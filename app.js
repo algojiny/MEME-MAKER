dc6f9eba97bfead7a024a19d76ee53e047b07717
Jiny <jiny8510@gmail.com>
2022-10-31 10:33:46 +0900

    #2.3 Paint Color part One (03:19)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const canvas = document.querySelector("canvas");
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const showLineWidth = document.getElementById("show-line-width");
+const showColorCode = document.getElementById("show-color");
 const ctx = canvas.getContext("2d"); //그림을 그릴때 붓처럼 사용하는것 context
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 showLineWidth.innerText = lineWidth.value + "px";
+showColorCode.innerText = color.value;
 let isPainting = false;
 
 // const colors = ["#ff3838", "#ffb8b8", "#c56cf8", "#ff9f1a", "#fff200", "#32ff7e", "#7efff5", "#18dcff", "#7d5fff"];
@@ -35,9 +38,16 @@ function onLineWidthChange(event) {
   showLineWidth.innerText = event.target.value + "px";
 }
 
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+  showColorCode.innerText = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); //마우스가 캔버스를 벗어나면 그리기모드 취소
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
